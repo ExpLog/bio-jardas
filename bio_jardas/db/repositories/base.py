d94aa85c015f23e4ff97e4b6ea37c70fe3a74fc2
Leonardo Fontoura <lobo.fontoura@gmail.com>
2025-09-24 17:39:52 +0100

    Adds all message model repositories and docstrings.

--- bio_jardas/db/repositories/base.py
+++ bio_jardas/db/repositories/base.py
@@ -1,4 +1,11 @@
-from sqlalchemy import ColumnElement, delete, exists, select
+
+from sqlalchemy import (
+    ColumnElement,
+    ColumnExpressionArgument,
+    delete,
+    exists,
+    select,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.base import ExecutableOption
 
@@ -19,7 +26,8 @@ class CRUDRepository[T: Base]:
         self._flush: bool = flush
 
     async def _flush_session(self, flush: bool | None = None):
-        if self._flush or flush:
+        flush = flush if flush is not None else self._flush
+        if flush:
             await self.session.flush()
 
     def _check_not_found(self, instance: T | None, identifier: str) -> None:
@@ -27,31 +35,63 @@ class CRUDRepository[T: Base]:
             raise EntityNotFoundError(self.model_type, identifier)
 
     async def add(self, entity: T, flush: bool | None = None) -> T:
+        """
+        Add entity to session.
+        :param entity: Model instance.
+        :param flush: Flush session. Overrides CRUDRepository flush setting if not None.
+        :return: The same entity that was passed into the function.
+        """
         self.session.add(entity)
         await self._flush_session(flush)
         return entity
 
     async def add_many(self, entities: list[T], flush: bool | None = None) -> list[T]:
+        """
+        Adds a list of entities to the session.
+        :param entities: A list of model instances.
+        :param flush: Flush session. Overrides CRUDRepository flush setting if not None.
+        :return: The same list of entities that was passed into the function.
+        """
         self.session.add(entities)
         await self._flush_session(flush)
         return entities
 
     async def delete(self, entity_id: int) -> bool:
+        """
+        Delete row by id.
+        :param entity_id: The id of the entity to be deleted.
+        :return: A row was deleted or not.
+        """
         query = delete(self.model_type).where(self.model_type.id == entity_id)
         result = await self.session.execute(query)
         return result.rowcount > 0
 
     async def delete_many(self, entity_ids: list[int]) -> int:
+        """
+        Delete many rows by id.
+        :param entity_ids: The list of ids of the entities to be deleted.
+        :return: How many rows were deleted.
+        """
         query = delete(self.model_type).where(self.model_type.id.in_(entity_ids))
         result = await self.session.execute(query)
         return result.rowcount
 
     async def delete_where(self, *filters: ColumnElement[bool]) -> int:
+        """
+        Delete many rows by filters.
+        :param filters: List of model filters.
+        :return: How many rows were deleted.
+        """
         query = delete(self.model_type).where(*filters)
         result = await self.session.execute(query)
         return result.rowcount
 
     async def exists(self, *filters: ColumnElement[bool]) -> bool:
+        """
+        Check if any row exists for the given list of filters.
+        :param filters: List of model filters.
+        :return: Rows exist or not.
+        """
         query = select(exists(self.model_type).where(*filters))
         return await self.session.scalar(query)
 
@@ -61,6 +101,13 @@ class CRUDRepository[T: Base]:
         options: list[ExecutableOption] | None = None,
         for_update: bool = False,
     ) -> T:
+        """
+        Get single model instance by id.
+        :param instance_id: The id of the entity to be fetched.
+        :param options: List of select.options, such as joinedload.
+        :param for_update: Lock database rows for update.
+        :return: Model instance. Raises EntityNotFoundError.
+        """
         instance = await self.get_one_or_none(
             self.model_type.id == instance_id, options=options, for_update=for_update
         )
@@ -73,6 +120,13 @@ class CRUDRepository[T: Base]:
         options: list[ExecutableOption] | None = None,
         for_update: bool = False,
     ) -> T:
+        """
+        Get single model instance by filters.
+        :param filters: List of model filters.
+        :param options: List of select.options, such as joinedload.
+        :param for_update: Lock database rows for update.
+        :return: Model instance. Raises EntityNotFoundError.
+        """
         instance = await self.get_one_or_none(
             *filters, options=options, for_update=for_update
         )
@@ -86,6 +140,13 @@ class CRUDRepository[T: Base]:
         options: list[ExecutableOption] | None = None,
         for_update: bool = False,
     ) -> T | None:
+        """
+        Get single model instance or nothing by filters.
+        :param filters: List of model filters.
+        :param options: List of select.options, such as joinedload.
+        :param for_update: Lock database rows for update.
+        :return: Model instance or None. Raises EntityNotFoundError.
+        """
         query = select(self.model_type).where(*filters)
         if options:
             query = query.options(*options)
@@ -96,10 +157,26 @@ class CRUDRepository[T: Base]:
     async def get_many(
         self,
         *filters: ColumnElement[bool],
+        order_by: list[ColumnExpressionArgument] | None = None,
+        limit: int = 20,
+        offset: int = 0,
         options: list[ExecutableOption] | None = None,
         for_update: bool = False,
     ) -> list[T]:
-        query = select(self.model_type).where(*filters)
+        """
+        Get list of model instances by filters.
+        :param order_by: Optional list of expressions to order the results by.
+        :param limit: Maximum number of rows to return.
+        :param offset: Number of rows to skip before returning results.
+        :param filters: List of model filters.
+        :param options: Optional list of SQLAlchemy loader options
+            (e.g. `selectinload`).
+        :param for_update: Lock database rows for update.
+        :return: Model instance. Raises EntityNotFoundError.
+        """
+        query = select(self.model_type).where(*filters).limit(limit).offset(offset)
+        if order_by:
+            query = query.order_by(*order_by)
         if options:
             query = query.options(*options)
         if for_update:
--- bio_jardas/db/repositories/message.py
+++ bio_jardas/db/repositories/message.py
@@ -4,7 +4,8 @@ from sqlalchemy import delete, exists, func, select
 from sqlalchemy.orm import joinedload
 
 from bio_jardas.db import Message, MessageGroup, MessageGroupChoice
-from bio_jardas.db.repositories.base import Repository
+from bio_jardas.db.exceptions import EntityNotFoundError
+from bio_jardas.db.repositories.base import CRUDRepository, Repository
 
 
 class MessageRepo(Repository):
@@ -112,3 +113,45 @@ class MessageRepo(Repository):
             .limit(1)
         )
         return await self.session.scalar(query)
+
+
+class MessageRepository(CRUDRepository[Message]):
+    model_type = Message
+
+    async def get_random(self, message_group_id: int) -> Message:
+        """
+        Get a random message from a message group.
+        :param message_group_id: A MessageGroup.id.
+        :return: A single message.
+        """
+        messages = await self.get_many(
+            Message.group_id == message_group_id, order_by=func.random(), limit=1
+        )
+        if not messages:
+            raise EntityNotFoundError(Message, "random")
+        return messages[0]
+
+
+class MessageGroupRepository(CRUDRepository[MessageGroup]):
+    model_type = MessageGroup
+
+    async def get_assigned_message_groups(
+        self, snowflake_id: int
+    ) -> list[MessageGroup]:
+        """
+        Get the MessageGroups that are assigned to the snowflake_id through a
+        MessageGroupChoice.
+        :param snowflake_id: Discord snowflake id of a channel or user.
+        :return: List of message groups.
+        """
+        query = (
+            select(MessageGroup)
+            .join(MessageGroupChoice)
+            .where(MessageGroupChoice.snowflake_id == snowflake_id)
+        )
+
+        return list((await self.session.scalars(query)).all())
+
+
+class MessageGroupChoiceRepository(CRUDRepository[MessageGroupChoice]):
+    model_type = MessageGroupChoice
